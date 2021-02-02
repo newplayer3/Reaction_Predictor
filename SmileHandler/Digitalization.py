044843765eb51a3d86e4e152a071e3a9cc772be2
wenyuan <newgameplayer3@gmail.com>
2021-02-01 22:27:28 -0400

    timeProblem

--- SmileHandler/Digitalization.py
+++ SmileHandler/Digitalization.py
@@ -1,18 +1,25 @@
-import torch 
-import numpy as np
 import re
-
-class Digitalization():
+ 
+      
+def chainNumerical( str ):
+    smile = str
+    pattern1 = r':[0-9]+'
+    smile = re.sub(pattern1, '', smile)
+    pattern2 = '[A-Z]'
+    smile = re.sub(pattern2, lambda x: ' '+x.group(0), smile)
+    pattern3 = '[0-9]'
+    smile = re.sub(pattern3, lambda x: ' '+x.group(0),smile)
     
-    def __init__(self, Main, side, connection):
-        self._connection = connection
-        self._sidechain = side
-        self._mainchain = Main
-        self._digit = torch.Tensor(self._connection.count('[A-Z]', 10)
-        
-    
+    return smile
         
         
-        
+if __name__ == '__main__':
+    s1 = r'Cl'
+    s2 = r'CBaVi[OH:2][C@@H:3]|[CH:4]=[CH:5][CH:6]1[CH2:10][CH2:9][C:8]|[CH:7]1[CH2:12][CH:13]=[CH:14][CH2:15][CH2:16][CH2:17][C:18]|=[O:19]'
+    s3 = r'C(O)C'
+    s4 = ['[CH2:21][CH2:22][CH2:23][CH2:24][CH3:25]', '=[O:11]', '[OH:20]']
+    s5 = r'_____(_____)_=__1___(=_)_1__=_____(_)=_' 
+    print(chainNumerical(s1))
+    print(chainNumerical(s2))    
         
         
